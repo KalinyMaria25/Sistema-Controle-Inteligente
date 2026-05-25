b678f12cab10d955aaef99aacf4e87cdcb9c736e
KalinyMaria25 <sousaliny54@gmail.com>
2026-05-24 23:08:47 -0300

    Update dados.py
    Sistema Financeiro em Python com Tkinter e SQLite
    
    Projeto de controle financeiro desenvolvido em Python utilizando Tkinter para interface gráfica e SQLite para armazenamento de dados.
    
    O sistema permite:
    
    • Cadastro e login de usuários
    • Exclusão de conta
    • Adição de receitas e despesas
    • Controle de saldo financeiro
    • Listagem de transações
    • Relatórios financeiros
    • Estatísticas de receitas e despesas
    • Gráficos financeiros com Matplotlib
    
    Tecnologias utilizadas:
    
    * Python
    * Tkinter
    * SQLite
    * Matplotlib

--- dados.py
+++ dados.py
@@ -1,5 +1,43 @@
-usuarios = []
-transacoes = []
-lixeira = []
+import sqlite3
 
+# ================= BANCO =================
 
+conexao = sqlite3.connect("financeiro.db")
+
+cursor = conexao.cursor()
+
+# ================= TABELA USUÁRIOS =================
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS usuarios(
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    nome TEXT,
+
+    email TEXT UNIQUE,
+
+    senha TEXT
+)
+""")
+
+# ================= TABELA TRANSAÇÕES =================
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS transacoes(
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    tipo TEXT,
+
+    valor REAL,
+
+    descricao TEXT,
+
+    data TEXT
+)
+""")
+
+# ================= SALVAR =================
+
+conexao.commit()
