f56d53d2ebc5a888b2a2cc53bfaef0fb998fb90b
Camila390 <camilamouramoura390@gmail.com>
2026-05-24 23:13:48 -0300

    Update main.py
    Inicialização e Encerramento do Sistema Financeiro
    
    Este arquivo é responsável pelo controle principal de execução da aplicação financeira desenvolvida em Python com Tkinter. Ele realiza a inicialização da interface gráfica e garante o fechamento correto do sistema e da conexão com o banco de dados.
    
    Funcionalidades:
    
    Importação da interface principal
    Gerenciamento da conexão com banco de dados
    Encerramento seguro da aplicação
    Fechamento automático da conexão ao sair do sistema
    Execução da janela principal com Tkinter

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from interface import *
+from interface import janela
+from dados import conexao
+
+# ================= FECHAR =================
+
+def fechar_sistema():
+
+    conexao.close()
+
+    janela.destroy()
+
+janela.protocol(
+    "WM_DELETE_WINDOW",
+    fechar_sistema
+)
+
+# ================= EXECUTAR =================
+
+janela.mainloop()
